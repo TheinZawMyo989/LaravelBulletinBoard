fe57821cdfaa2a6ccd356eb61026079cbaa44577
Thein Zaw Myo <theinzawmyo@cgm-myanmar.com>
2020-03-19 14:09:36 +0630

    commit

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -1,10 +1,10 @@
-<div class="js-cookie-consent cookie-consent text-center fixed-bottom p-2 bg-info">
+<div class="js-cookie-consent cookie-consent text-center fixed-bottom p-2 border-top bg-white">
 
-    <span class="cookie-consent__message text-white">
+    <span class="cookie-consent__message">
         {!! trans('cookieConsent::texts.message') !!}
     </span>
 
-    <button class="js-cookie-consent-agree cookie-consent__agree btn btn-warning align-self-center">
+    <button class="js-cookie-consent-agree cookie-consent__agree btn btn-success align-self-center">
         {{ trans('cookieConsent::texts.agree') }}
     </button>
 
